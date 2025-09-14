b704859f4784cc441452e5768c822441d9663a38
M ALI RAZA <casim9530@gmail.com>
2025-09-14 00:36:12 -0700

    auto typing

--- auto typing text/script.js
+++ auto typing text/script.js
@@ -1,9 +1,32 @@
-let textValues = ["ali", "raza", "asim", "ashu"];
+let textValues = ["coder", "developer", "youtuber"];
 const spanValue = document.querySelector("span");
-let word = "aliraza";
+let word = 0;
 let character = 0;
+
+let reversetype = false;
+
 let intervalId = setInterval(() => {
-  if (character === word.length - 1) clearInterval(intervalId);
-  spanValue.innerText = spanValue.innerText + word[character];
-  character++;
-}, 500);
+  if (character === textValues[word].length) {
+    reversetype = true;
+  }
+
+  if (!reversetype) {
+    spanValue.innerText = spanValue.innerText + textValues[word][character];
+    character++;
+  } else {
+    spanValue.innerText = spanValue.innerText.slice(
+      0,
+      spanValue.innerText.length - 1
+    );
+  }
+
+  if (spanValue.innerText.length === 0 && reversetype) {
+    reversetype = false;
+    character = 0;
+    word++;
+  }
+
+  if (word === textValues.length) {
+    word = 0;
+  }
+}, 200);
